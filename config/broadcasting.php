24536926ebaaf7342b193cf8bb1ac367d859a46e
Tats <tatsuya01a1@gmail.com>
2023-05-05 17:02:44 +0900

    timeout fix test#01

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -41,6 +41,8 @@ return [
                 'scheme' => env('PUSHER_SCHEME', 'https'),
                 'encrypted' => true,
                 'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
+                'activityTimeout' => 1000,
+                'pongTimeout' => 40000,
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
